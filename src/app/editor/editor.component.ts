618a063fca2e64167084404c58ca20aa37b2004c
yuriy_chaban <yuriy.chaban@masterofcode.com>
2017-10-05 17:08:04 +0300

    try to use change on editor

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges, AfterViewInit, ViewChild } from '@angular/core';
 
 declare var JSONEditor;
 
@@ -7,19 +7,37 @@ declare var JSONEditor;
     templateUrl: './editor.component.html',
     styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements OnInit, OnChanges, AfterViewInit {
+
+    @ViewChild('jsoneditor') jsonEditor;
+
+    @Input() data;
 
     textEditor: any;
     treeEditor: any;
     editorRef: any;
 
-    constructor() {
+    public json = this.data;
+
+    constructor() {}
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (!!changes['data'] && changes['data'].currentValue != null) {
+            this.editorRef.set(this.data);
+        }
+
+        this.ngAfterViewInit();
+
+        console.log("ololo");
     }
 
     ngOnInit() {
-        this.textEditor = document.getElementById('text-editor');
-        this.treeEditor = document.getElementById('tree-editor');
+        this.textEditor = this.jsonEditor.nativeElement;
         this.createTextObjectViewer();
+    }
+
+    ngAfterViewInit() {
+        this.treeEditor = document.getElementById('tree-editor');
         this.createTreeObjectViewer();
     }
 
@@ -30,13 +48,35 @@ export class EditorComponent implements OnInit {
         };
 
         let json = {
-            "Array": [1, 2, 3],
-            "Boolean": true,
-            "Null": null,
-            "Number": 123,
-            "Object": {"a": "b", "c": "d"},
-            "String": "Hello World"
-        };
+            "widget": {
+                "debug": "on",
+                "window": {
+                    "title": "Sample Konfabulator Widget",
+                    "name": "main_window",
+                    "width": 500,
+                    "height": 500
+                },
+                "image": {
+                    "src": "Images/Sun.png",
+                    "name": "sun1",
+                    "hOffset": 250,
+                    "vOffset": 250,
+                    "alignment": "center"
+                },
+                "text": {
+                    "data": "Click Here",
+                    "size": 36,
+                    "style": "bold",
+                    "name": "text1",
+                    "hOffset": 250,
+                    "vOffset": 100,
+                    "alignment": "center",
+                    "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+                }
+            }
+        };;
+
+        console.log('createTextObjectViewer: ', this.json);
 
         this.editorRef = new JSONEditor(this.textEditor, options, json);
     }
@@ -44,16 +84,39 @@ export class EditorComponent implements OnInit {
     createTreeObjectViewer() {
         let options = {
             "mode": "view",
+            "search": false,
         };
 
         let json = {
-            "Array": [1, 2, 3],
-            "Boolean": true,
-            "Null": null,
-            "Number": 123,
-            "Object": {"a": "b", "c": "d"},
-            "String": "Hello World"
+            "widget": {
+                "debug": "on",
+                    "window": {
+                    "title": "Sample Konfabulator Widget",
+                        "name": "main_window",
+                        "width": 500,
+                        "height": 500
+                },
+                "image": {
+                    "src": "Images/Sun.png",
+                        "name": "sun1",
+                        "hOffset": 250,
+                        "vOffset": 250,
+                        "alignment": "center"
+                },
+                "text": {
+                    "data": "Click Here",
+                        "size": 36,
+                        "style": "bold",
+                        "name": "text1",
+                        "hOffset": 250,
+                        "vOffset": 100,
+                        "alignment": "center",
+                        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+                }
+            }
         };
+
+        console.log('createTreeObjectViewer: ', this.json);
 
         this.editorRef = new JSONEditor(this.treeEditor, options, json);
     }
